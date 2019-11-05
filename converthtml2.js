f47ebb6efb43500fb9f19eda46a43b6d8a6e903e
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-11-04 19:52:52 -0500

    updated

--- converthtml2.js
+++ converthtml2.js
@@ -1,14 +1,12 @@
-// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
-// using replca method
+  // the backslash g will replace all globally 
 function convertHTML(str) {
     // &colon;&rpar;
-    
-    str =  str.replace(/&/g, "&amp;");
-    str = str.replace(/</g,"&lt");
-    str = str.replace(/>/g, "&gt");    
-    str = str.replace(/"/g, "&quot")
-    str = str.replace(/abc/g, "&quot"); 
-    
+    str = str.replace(/&/g, "&amp;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/>/g, "&gt;");
+    str = str.replace(/"/g, "&quot;");
+    str = str.replace(/'/g, "&apos;");
     return str;
-    }
+  }
   
+  convertHTML("Dolce & Gabbana");
