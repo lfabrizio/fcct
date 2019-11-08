8a6106c9372d2dd9c2509734512139e31d3d09a2
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-11-07 19:00:09 -0500

    smallest common

---
+++ scmultiple.js
@@ -0,0 +1,31 @@
+// algorithm challenge 
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+// an array of numbers
+// use sort method and modulo
+// algorithm challenge 
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+// an array of numbers
+// use sort method and modulo
+function smallestCommons(arr) {
+
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let addIt = max;
+    // console.log(smallNum);
+    // console.log(largeNum);
+    // console.log(addIt);
+
+    for(let i = max - 1; i >= min;i--) {
+
+        if(addIt % i){
+        addIt += max;
+        i = max;
+        }
+    }
+   
+    return addIt;
+  }
